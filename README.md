b8e562ffa6436026f2c6e47ec31598585fc6d02c
Antonio Freire <antonio.freire@daimler.com>
2019-02-08 10:16:31 +0000

    feat(backend): flask backend server and requirements

--- README.md
+++ README.md
@@ -7,6 +7,15 @@
 ## Setup
 
 - Clone the repo
+- cd into the dir/
+- Open 2 terminal tabs
+
+Backend (from root):
+
+- `python3 -m venv venv` for a virtual environment within
+- `source venv/bin/activate`
+- `pip install -r requirements.txt` to install the required packages
+- `python3 app.py` to launch the backend server
 
 Frontend (from client/ dir):
 
---
+++ app.py
@@ -0,0 +1,37 @@
+from eventlet import wsgi
+import eventlet
+from threading import Lock
+from flask import Flask, request
+from flask_socketio import SocketIO, emit, disconnect
+
+async_mode = None
+
+app = Flask(__name__)
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+thread_lock = Lock()
+
+def background_thread():
+    count = 0
+    data = []
+    while True:
+        socketio.sleep(3)
+        count += 1
+        data.append(count)
+        socketio.emit(
+            'response', {'connected': True, 'data': data, 'count': count})
+
+@socketio.on('connect')
+def test_connect():
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(background_thread)
+    emit('response', {'data': 'Connected', 'count': 0})
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected', request.sid)
+
+if __name__ == '__main__':
+    wsgi.server(eventlet.listen(('', 5000)), app)
