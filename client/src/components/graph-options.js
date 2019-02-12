a5f0996f233b0fce4b147cf3d803c39bbec20152
Antonio Freire <antonio.freire@daimler.com>
2019-02-12 18:13:43 +0000

    feat(data-vis): add chart component

---
+++ client/src/components/graph-options.js
@@ -0,0 +1,95 @@
+const options = {
+  chart: {
+    animation: true,
+    type: 'line',
+    backgroundColor: {
+      linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
+      stops: [[0, '#2a2a2b'], [1, '#272727']]
+    },
+    style: {
+      fontFamily: "'Roboto Slab', serif"
+    }
+  },
+  xAxis: {
+    type: '',
+    gridLineColor: '#707073',
+    labels: {
+      enabled: false,
+      style: {
+        color: '#E0E0E3'
+      }
+    },
+    lineColor: '#707073',
+    minorGridLineColor: '#505053',
+    tickColor: '#707073',
+    title: {
+      style: {
+        color: '#A0A0A3'
+      }
+    }
+  },
+  yAxis: {
+    gridLineColor: '#707073',
+    labels: {
+      style: {
+        color: '#E0E0E3'
+      }
+    },
+    lineColor: '#707073',
+    minorGridLineColor: '#505053',
+    tickColor: '#707073',
+    tickWidth: 1,
+    title: {
+      text: 'Values',
+      style: {
+        color: '#A0A0A3',
+        fontSize: '12px'
+      }
+    },
+    plotLines: [
+      {
+        value: 0,
+        width: 1
+      }
+    ]
+  },
+  title: {
+    text: 'Accelerometer',
+    style: { color: '#FFF', fontSize: '14px' }
+  },
+  legend: {
+    itemStyle: {
+      color: '#E0E0E3'
+    },
+    itemHoverStyle: {
+      color: '#FFF'
+    },
+    itemHiddenStyle: {
+      color: '#606063'
+    }
+  },
+  credits: {
+    style: {
+      color: '#272727'
+    }
+  },
+  series: [
+    {
+      name: 'X',
+      color: '#37c871',
+      data: []
+    },
+    {
+      name: 'Y',
+      color: '#fff',
+      data: []
+    },
+    {
+      name: 'Z',
+      color: '#DE3163',
+      data: []
+    }
+  ]
+}
+
+export default options
--- client/src/containers/data-viz.jsx
+++ client/src/containers/data-viz.jsx
@@ -1,7 +1,10 @@
 import React, { PureComponent } from 'react'
 import { shape, func, bool } from 'prop-types'
 import { connect } from 'react-redux'
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
 import Navbar from '../components/navbar.jsx'
+import options from '../components/graph-options'
 import { getData, getStatus, watchData } from '../duxs/data-viz'
 
 class DataViz extends PureComponent {
@@ -20,15 +23,112 @@ class DataViz extends PureComponent {
   constructor(props) {
     super(props)
 
+    this.state = {
+      options: {
+        ...options
+      }
+    }
+
     this.props.watchData()
   }
 
+  renderAccelerometerOptions = data => ({
+    ...options,
+    chart: {
+      ...options.chart,
+      className: 'Dataviz-graph'
+    },
+    yAxis: {
+      ...options.yAxis,
+      min: -1.5,
+      max: 1.5
+    },
+    series: [
+      ...options.series.map((serie, index) =>
+        index === 0
+          ? {
+              ...serie,
+              data: [...data.accelerometer.map(obj => obj.x)],
+              animation: false
+            }
+          : index === 1
+          ? {
+              ...serie,
+              data: [...data.accelerometer.map(obj => obj.y)],
+              animation: false
+            }
+          : {
+              ...serie,
+              data: [...data.accelerometer.map(obj => obj.z)],
+              animation: false
+            }
+      )
+    ]
+  })
+
+  renderGyroscopeOptions = data => ({
+    ...options,
+    title: {
+      text: 'Gyroscope',
+      style: { color: '#FFF', fontSize: '14px' }
+    },
+    chart: {
+      ...options.chart,
+      className: 'Dataviz-graph'
+    },
+    yAxis: {
+      ...options.yAxis,
+      min: -1,
+      max: 1
+    },
+    series: [
+      ...options.series.map((serie, index) =>
+        index === 0
+          ? {
+              ...serie,
+              name: 'Roll',
+              data: [...data.gyroscope.map(obj => obj.roll)],
+              animation: false
+            }
+          : index === 1
+          ? {
+              ...serie,
+              name: 'Pitch',
+              data: [...data.gyroscope.map(obj => obj.pitch)],
+              animation: false
+            }
+          : {
+              ...serie,
+              name: 'Yaw',
+              data: [...data.gyroscope.map(obj => obj.yaw)],
+              animation: false
+            }
+      )
+    ]
+  })
+
   render() {
-    const { connected } = this.props
+    const { connected, data } = this.props
 
     return (
-      <div>
+      <div className="Dataviz">
         <Navbar connected={connected} />
+
+        <div className="Dataviz-wrapper">
+          <HighchartsReact
+            highcharts={Highcharts}
+            updateArgs={[true, true, true]}
+            options={this.renderAccelerometerOptions(data)}
+          />
+        </div>
+
+        <div className="Dataviz-wrapper">
+          <HighchartsReact
+            highcharts={Highcharts}
+            updateArgs={[true, true, true]}
+            options={this.renderGyroscopeOptions(data)}
+          />
+        </div>
       </div>
     )
   }
